02c1519f811c2f6387d97dc800d5e9b03ed84c32
Aram Leblebjian <aramleblebjian@Arams-MacBook-Pro.local>
2025-04-26 00:46:30 +0400

    gemini change

--- simulate.py
+++ simulate.py
@@ -2,10 +2,10 @@ from game import MafiaGame
 import json
 import random
 
-allSame = False
+allSame = True
 
 if allSame:
-    mafia_game = MafiaGame("openai") #openai/gemini/grok working    -    claude/deepseek not working as expected with the prompts
+    mafia_game = MafiaGame("gemini")
     mafia_game.run()
 else:
     llm_names = 2 * ["openai"] + 2 * ["gemini"] + 2 * ["grok"] + 2 * ["claude"] + 2 * ["deepseek"]
@@ -15,5 +15,3 @@ else:
 
 with open("results.json", "w") as json_file:
     json.dump(mafia_game.game_data, json_file, indent=4)
-
-# Joseph to test why claude and deepseek are not following orders from prompts. ok bro
