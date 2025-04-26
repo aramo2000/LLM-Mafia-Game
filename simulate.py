e01ae1c39531d6d57ce80e1b876a0a6924286e8a
joseph-alakach <joseph-alakach@edu.aua.am>
2025-04-26 16:28:55 +0400

    adding llm vote stats

--- simulate.py
+++ simulate.py
@@ -6,14 +6,18 @@ allSame = False
 
 if allSame:
     mafia_game = MafiaGame("openai") #openai/gemini/grok working    -    claude/deepseek not working as expected with the prompts
-    mafia_game.run()
 else:
     llm_names = 2 * ["openai"] + 2 * ["gemini"] + 2 * ["grok"] + 2 * ["claude"] + 2 * ["deepseek"]
     random.shuffle(llm_names)
     mafia_game = MafiaGame.from_llm_list(llm_names)
+
+number_of_games = 1
+games_total_record = []
+for _ in range(number_of_games):
     mafia_game.run()
+    games_total_record.append(mafia_game.game_data)
 
 with open("results.json", "w") as json_file:
-    json.dump(mafia_game.game_data, json_file, indent=4)
+    json.dump(games_total_record, json_file, indent=4)
 
 # Joseph to test why claude and deepseek are not following orders from prompts. ok bro
